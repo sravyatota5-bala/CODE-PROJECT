39e8301786d09af49b10edc931f2e20d9ce9ec78
sravyatota5-bala <sravyatota5@gmail.com>
2025-11-04 15:11:34 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+//accessing array elements
+#include<stdio.h>
+int main()
+{
+	int a[3]={10,20,30};
+	printf("\n%d",a[0]);
+	printf("\n%d",a[1]);
+	printf("\n%d",a[2]);
+	printf("\n%d",a[3]);
+	return 0;
+	
+}

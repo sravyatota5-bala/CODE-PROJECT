e407f665bbd2caed2c3853aea2c1a4c631d963bc
sravyatota5-bala <sravyatota5@gmail.com>
2025-11-15 20:03:05 +0530

    Add files via upload

---
+++ accesing array tab space.c
@@ -0,0 +1,13 @@
+//accessing array elements using tab space 
+#include<stdio.h> 
+int main() 
+{ 
+int a[3]={10,20,30}; 
+int i; 
+printf("\n The array elements are\n"); 
+for(i=0;i<3;i++) 
+{ 
+printf("%d\t",a[i]); 
+}
+return 0;
+}
